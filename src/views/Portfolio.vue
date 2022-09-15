37cf3fe3d8ee8116726b0d7580ce382ad97ba4ef
Ananthu Krishnan <ananthukrish.k@gmail.com>
2022-09-15 23:13:10 +0530

    Home page response added

--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -1,35 +1,28 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import bgImf from "@/assets/images/screenshot.png";
+</script>
 
 <template>
   <div class="portfolio pd-screen" id="portfolio">
     <h1 class="text-6xl uppercase font-bold py-10">Portfolio</h1>
     <div class="row">
-      <div class="col col-sm-6 col-lg-4 mt-5">
-        <div class="portfolio__img-container">
-          <img
-            src="../assets/images/screenshot.png"
-            class="portfolio__img"
-            alt=""
-          />
-        </div>
+      <div class="col col-sm-6 col-lg-4 mt-10 sm:mt-5">
+        <div
+          class="portfolio__img-container box1"
+          :style="{ backgroundImage: `url(${bgImf})` }"
+        ></div>
       </div>
-      <div class="col col-sm-6 col-lg-4 mt-5">
-        <div class="portfolio__img-container">
-          <img
-            src="../assets/images/screenshot.png"
-            class="portfolio__img"
-            alt=""
-          />
-        </div>
+      <div class="col col-sm-6 col-lg-4 mt-10 sm:mt-5">
+        <div
+          class="portfolio__img-container box2"
+          :style="{ backgroundImage: `url(${bgImf})` }"
+        ></div>
       </div>
-      <div class="col col-sm-6 col-lg-4 mt-5">
-        <div class="portfolio__img-container">
-          <img
-            src="../assets/images/screenshot.png"
-            class="portfolio__img"
-            alt=""
-          />
-        </div>
+      <div class="col col-sm-6 col-lg-4 mt-10 sm:mt-5">
+        <div
+          class="portfolio__img-container box3"
+          :style="{ backgroundImage: `url(${bgImf})` }"
+        ></div>
       </div>
     </div>
   </div>
