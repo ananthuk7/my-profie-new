9fd1d44027ea687ad6e48684f42fff8b43793ad9
Ananthu Krishnan <ananthukrish.k@gmail.com>
2022-09-15 21:51:34 +0530

    about page response added

--- src/views/About.vue
+++ src/views/About.vue
@@ -4,8 +4,8 @@ import { onMounted, ref } from "vue";
 <template>
   <div id="about" class="about">
     <div class="about__child">
-      <h3 class="text-5xl font-bold mb-8">About Me</h3>
-      <p class="text-slate-800 text-2xl text-center font-semibold">
+      <h3 class="text-4xl md:text-5xl font-bold mb-8 mt-4">About Me</h3>
+      <p class="text-slate-800 text-left text-xl sm:text-2xl sm:text-center font-semibold">
         I'm a web developer and UI/UX developer. I love to solve design
         problems, develop new features and design prototypes using different
         mediums. My work is mainly focused on frontend-development. As for
