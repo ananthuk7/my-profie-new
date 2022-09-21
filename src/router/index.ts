cd99cbc03d9bad3db5664de61c6e7b60c83f2402
Ananthu Krishnan <ananthukrish.k@gmail.com>
2022-09-21 15:52:30 +0530

    bug fix

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+declare module 'vue-router' {
+  interface RouteMeta {
+    transition: string;
+  }
+}
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
