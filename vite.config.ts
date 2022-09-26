6f550a76f8fbf5ab592bc994010282ec2e880f4a
Ananthu Krishnan <ananthukrish.k@gmail.com>
2022-09-26 14:17:08 +0530

    vite rollup fix

--- vite.config.ts
+++ vite.config.ts
@@ -12,4 +12,11 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
+  build: {
+    rollupOptions: {
+      external: [
+        /^node:.*/,
+      ]
+    }
+  },
 });
